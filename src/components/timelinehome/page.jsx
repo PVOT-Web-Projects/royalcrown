b47f55d6b8daff2f2513aab214ae64365a9c4a93
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-08-02 19:09:32 +0530

    commit

--- src/components/timelinehome/page.jsx
+++ src/components/timelinehome/page.jsx
@@ -1,22 +1,71 @@
-import React from 'react';
-import './timelinehome.scss';
+"use client";
+import React, { useRef } from "react";
+import "./timelinehome.scss";
+import { motion } from "framer-motion";
 
 const TimelineHome = () => {
+  const scrollRef1 = useRef(null);
+  const scrollRef2 = useRef(null);
+  const scrollRef3 = useRef(null);
+
   return (
     <div className="container">
       <div className="content">
-        <div className="Timelinetext">We are known for providing high density laminates with unparalleled services in terms of quality and reliability. We are known for providing high density laminates with unparalleled services in terms of quality and reliability.</div>
-        <div className="Timelinecircle"></div>
+        <motion.div
+          className="Timelinetext1"
+          initial={{ x: 200, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
+          viewport={{ once: true }}
+        >
+          We are known for providing high density laminates with unparalleled
+          services in terms of quality and reliability. We are known for
+          providing high density laminates with unparalleled services in terms
+          of quality and reliability.
+        </motion.div>
+        <motion.div
+          ref={scrollRef1}
+          className="Timelinecircle1"
+          initial={{ x: 0, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
+          viewport={{ once: true }}
+        ></motion.div>
       </div>
       <div className="content1">
-        <div className="Timelinecircle"></div>
-        </div>
+        <motion.div
+          className="Timelinecircle2"
+          initial={{ x: 0, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
+          viewport={{ once: true }}
+        ></motion.div>
+      </div>
       <div className="content2">
-        <div className="Timelinetext">We are known for providing high density laminates with unparalleled services in terms of quality and reliability.</div>
-        <div className="Timelinecircle"></div>
+        <motion.div
+          className="Timelinetext2"
+          initial={{ x: -200, opacity: 0 }}
+          whileInView={{ x: 100, opacity: 1 }}
+          transition={{ duration: 2 }}
+          viewport={{ once: true }}
+        >
+          We are known for providing high density laminates with unparalleled
+          services in terms of quality and reliability. We are known for
+          providing high density laminates with unparalleled services in terms
+          of quality and reliability.
+        </motion.div>
+
+        <motion.div
+          ref={scrollRef3}
+          className="Timelinecircle3"
+          initial={{ x: 0, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 2 }}
+          viewport={{ once: true }}
+        ></motion.div>
       </div>
     </div>
   );
-}
+};
 
 export default TimelineHome;
