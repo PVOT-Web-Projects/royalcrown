eed614ecda88eaa515c581cb1c7b5dd7af02b48d
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-02 12:22:00 +0530

    upd -j

---
+++ src/components/timelinehome/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './timelinehome.scss';
+
+const TimelineHome = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="text">We are known for providing high density laminates with unparalleled services in terms of quality and reliability.</div>
+        <div className="circle"></div>
+      </div>
+      <div className="content">
+        <div className="circle"></div>
+        {/* <div className="text">We are known for providing high density laminates with unparalleled services in terms of quality and reliability.</div> */}
+      </div>
+      <div className="content">
+        <div className="text">We are known for providing high density laminates with unparalleled services in terms of quality and reliability.</div>
+        <div className="circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default TimelineHome;
