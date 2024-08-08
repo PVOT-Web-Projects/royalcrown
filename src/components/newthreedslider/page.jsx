727ed71ab066d8875943ddf2f8cc468d0331d2a6
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-08 11:31:45 +0530

    new chnages -j

--- src/components/newthreedslider/page.jsx
+++ src/components/newthreedslider/page.jsx
@@ -2,7 +2,9 @@
 "use client";
 import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/swiper-bundle.min.css";
 import "swiper/css";
+// import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
@@ -24,30 +26,33 @@ import {
   EffectFade,
   EffectCoverflow,
 } from "swiper/modules";
-import styles from "./newthreedslider.module.css";
+import "@/components/newthreedslider/newthreedslider.scss";
 import SliderBtn from "@/components/buttons/sliderbtn/page";
+
+
 
 const SwiperComponent = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
-    <div className={styles.MainSwiperthreedContainer}>
-      <div className={styles.ThreeText}>
-        <p className={styles.ThreeTextInner}>The Pinnacle of Luxury Laminates</p>
-        <p className={styles.ThreeTextInnerFirst}>
+    <div className="MainSwiperthreedContainer">
+      <div className="ThreeText">
+        <p className="ThreeTextInner">The Pinnacle of Luxury Laminates</p>
+        <p className="ThreeTextInnerFirst">
           Discover the epitome of sophistication with our premium range of
           luxury laminates. Designed for those with discerning taste, our
           laminates offer a perfect blend of elegance and durability,
           transforming ordinary spaces into extraordinary masterpieces.
         </p>
       </div>
-      <div className={styles.swiperContainerOne}>
+      <div className="swiper-container-one">
         <Swiper
           modules={[Navigation, EffectCoverflow, Thumbs]}
           slidesPerView={3}
           loop={true}
           effect="coverflow"
           centeredSlides={true}
+          // spaceBetween={400}
           coverflowEffect={{
             rotate: 0,
             stretch: 0,
@@ -58,160 +63,208 @@ const SwiperComponent = () => {
           thumbs={{ swiper: thumbsSwiper }}
           speed={1500}
           navigation={{
-            nextEl: `.${styles.swiperButtonNext}`,
-            prevEl: `.${styles.swiperButtonPrev}`,
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
           }}
         >
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Decorative</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Decorative</p>
               </div>
               <Image
                 src={Img1}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Xylem</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Xylem</p>
               </div>
               <Image
                 src={Img2}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Compact</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Compact</p>
               </div>
               <Image
                 src={Img3}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Decorative</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Decorative</p>
               </div>
               <Image
                 src={Img1}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Xylem</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Xylem</p>
               </div>
               <Image
                 src={Img2}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className={styles.SliderThreeContent}>
-              <div className={styles.SliderThreeContentTEXT}>
-                <p className={styles.SliderThreeContentTEXTInner}>Compact</p>
+            <div className="SliderThreeContent">
+              <div className="SliderThreeContentTEXT">
+                <p className="SliderThreeContentTEXTInner">Compact</p>
               </div>
               <Image
                 src={Img3}
                 alt="carousel_image"
-                className={styles.SliderThreeImages}
+                className="SliderThreeImages"
               />
             </div>
           </SwiperSlide>
-          <div className={styles.swiperButtonNext}>
-            <Image src={SvgBtnNext} alt="btn" className={styles.SvgBtnSlider} />
+          <div className="swiper-button-next">
+            <Image src={SvgBtnNext} alt="btn" className="SvgBtnSlider" />
           </div>
-          <div className={styles.swiperButtonPrev}>
-            <Image src={SvgBtn} alt="btn" className={styles.SvgBtnSlider} />
+          <div className="swiper-button-prev">
+            <Image src={SvgBtn} alt="btn" className="SvgBtnSlider" />
           </div>
         </Swiper>
-        <div className={styles.SecondThumbsSwiper}>
+        {/* <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
+      <div className="swiper-pagination"></div> */}
+
+        <div className="SecondThumbsSwiper">
           <Swiper
             modules={[Thumbs, Autoplay, EffectFade]}
             onSwiper={setThumbsSwiper}
+            //   loop={true}
+            // autoplay={{
+            //   delay: 2500,
+            // }}
+            // navigation={true}
             slidesPerView={1}
             speed={1500}
+            // navigation={{
+            //   nextEl: ".swiper-button-next",
+            //   prevEl: ".swiper-button-prev",
+            // }}
+            // pagination={{
+            //   dynamicBullets: true,
+            // }}
             allowTouchMove={false}
             fadeEffect={{ crossFade: true }}
-            className={styles.mySwiper2}
+            className="mySwiper2"
           >
             <SwiperSlide>
-              <div className={styles.CarouselTextMaincontent}>
-                <p className={styles.TextInnerText}>
+              <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
                   It's a modern minimalist aesthetic look, our luxury laminates
                   cater to every desire, ensuring your interiors exude a refined
                   charm that captivates and endures.
                 </p>
-                <p className={styles.CarouselTextContent}>
+                <p className="carousel_text_Content">
                   Elevate your space with the ultimate expression of luxury and
                   grace.
                 </p>
-                <div className={styles.SliderBtnOuter}>
+                <div className="SliderBtnOuter">
                    <SliderBtn url={"/"} btn_text={"GO TO DECORATIVE"}/>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className={styles.CarouselTextMaincontent}>
-                <p className={styles.TextInnerText}>
+            <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
                   It's a modern minimalist aesthetic look, our luxury laminates
                   cater to every desire, ensuring your interiors exude a refined
                   charm that captivates and endures.
                 </p>
-                <p className={styles.CarouselTextContent}>
+                <p className="carousel_text_Content">
                   Elevate your space with the ultimate expression of luxury and
                   grace.
                 </p>
-                <div className={styles.SliderBtnOuter}>
+                <div className="SliderBtnOuter">
                    <SliderBtn url={"/"} btn_text={"GO TO XYLEM"}/>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className={styles.CarouselTextMaincontent}>
-                <p className={styles.TextInnerText}>
+            <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
                   It's a modern minimalist aesthetic look, our luxury laminates
                   cater to every desire, ensuring your interiors exude a refined
                   charm that captivates and endures.
                 </p>
-                <p className={styles.CarouselTextContent}>
+                <p className="carousel_text_Content">
                   Elevate your space with the ultimate expression of luxury and
                   grace.
                 </p>
-                <div className={styles.SliderBtnOuter}>
+                <div className="SliderBtnOuter">
                    <SliderBtn url={"/"} btn_text={"GO TO COMPACT"}/>
                 </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
-              <div className={styles.CarouselTextMaincontent}>
-                <p className={styles.TextInnerText}>
+            <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
                   It's a modern minimalist aesthetic look, our luxury laminates
                   cater to every desire, ensuring your interiors exude a refined
                   charm that captivates and endures.
                 </p>
-                <p className={styles.CarouselTextContent}>
+                <p className="carousel_text_Content">
                   Elevate your space with the ultimate expression of luxury and
                   grace.
                 </p>
-                <div className={styles.SliderBtnOuter}>
+                <div className="SliderBtnOuter">
                    <SliderBtn url={"/"} btn_text={"GO TO DECORATIVE"}/>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+            <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
+                  It's a modern minimalist aesthetic look, our luxury laminates
+                  cater to every desire, ensuring your interiors exude a refined
+                  charm that captivates and endures.
+                </p>
+                <p className="carousel_text_Content">
+                  Elevate your space with the ultimate expression of luxury and
+                  grace.
+                </p>
+                <div className="SliderBtnOuter">
+                   <SliderBtn url={"/"} btn_text={"GO TO XYLEM"}/>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+            <div className="Carousel_text_maincontent">
+                <p className="Text_InnerText">
+                  It's a modern minimalist aesthetic look, our luxury laminates
+                  cater to every desire, ensuring your interiors exude a refined
+                  charm that captivates and endures.
+                </p>
+                <p className="carousel_text_Content">
+                  Elevate your space with the ultimate expression of luxury and
+                  grace.
+                </p>
+                <div className="SliderBtnOuter">
+                   <SliderBtn url={"/"} btn_text={"GO TO COMPACT"}/>
                 </div>
               </div>
             </SwiperSlide>
