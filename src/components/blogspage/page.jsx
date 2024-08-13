05235fe1c10dae3cb0b70cae2beb130b7a7ff809
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-13 19:08:35 +0530

    dsds

--- src/components/blogspage/page.jsx
+++ src/components/blogspage/page.jsx
@@ -1,22 +1,32 @@
 "use client";
 import React from "react";
 import "./blogs.scss";
-import {motion} from "framer-motion";
-const BlogsPage = ({ text }) => {
+import { motion } from "framer-motion";
+const BlogsPage = ({ text, description }) => {
   return (
     <div className="BlogsMainContainer">
-      <p>{text}</p>
+      <div>
+        <p className="TitleBlogs">{text}</p>
+        <div>
+          <p className="DescriptionBlogs">{description}</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 const Blogs = () => {
   return (
-    <motion.div initial={{ y: 50, opacity: 0 }}
-    whileInView={{ y: 0, opacity: 1 }}
-    transition={{ duration: 1 }}
-    viewport={{ once: true }}>
-      <BlogsPage text="Blogs" />
+    <motion.div
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
+      <BlogsPage
+        text="Blogs"
+        description="Our furniture collections are meticulously designed to elevate your living spaces."
+      />
     </motion.div>
   );
 };
