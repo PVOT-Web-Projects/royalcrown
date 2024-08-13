ae720e72c4072703ffe9752972d900a54c6bbd4f
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-13 17:28:25 +0530

    DSDDS

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -1,7 +1,12 @@
+"use client"
+import { useEffect } from "react";
 import Privacy_policy from "@/components/policyData/page";
 import PrivacyPolicyImg from "@/components/privacypolicyheader/page";
 
 const PrivacyPolicy =() =>{
+    useEffect(() => {
+        document.title = "Privacy Policy | Royal Crown";
+      });
     return(
         <div>
             <PrivacyPolicyImg />
--- src/app/vigil-mechanism-policy/page.jsx
+++ src/app/vigil-mechanism-policy/page.jsx
@@ -1,7 +1,12 @@
+"use client"
+import { useEffect } from "react";
 import Privacy_policy from "@/components/policyData/page";
 import PrivacyPolicyImg from "@/components/privacypolicyheader/page";
 
 const VigilMechanismPolicy =() =>{
+    useEffect(() => {
+        document.title = "Vigil Mechanism Policy | Royal Crown";
+      });
     return(
         <div>
             <PrivacyPolicyImg />
