694b6890c0b1d3a3e6b12042ced3bef34089f6a7
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-13 12:51:39 +0530

    find store -j

--- src/components/buttons/findstoreButton/findstoreButton.jsx
+++ src/components/buttons/findstoreButton/findstoreButton.jsx
@@ -2,8 +2,8 @@ import "./findstoreButton.scss"
 
 const FindStoreButton = ({ btn_text, OnClickSearch }) => {
   return (
-    <button type="submit" className="yello_btn">
-      <span className="button-content" onClick={OnClickSearch}>{btn_text} </span>
+    <button type="submit" className="yello_btn" onClick={OnClickSearch}>
+      <span className="button-content" >{btn_text} </span>
     </button>
   );
 };
