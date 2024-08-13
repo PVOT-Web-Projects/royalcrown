f796dc5f643b4be7a536585c4b1f985b20289928
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-08-13 15:17:17 +0530

    blogss -j

--- src/components/blogPosts/page.jsx
+++ src/components/blogPosts/page.jsx
@@ -6,6 +6,8 @@ import Data from "./data";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import BlogsCards from "@/components/blogPosts/blogcards";
+import YellowSubmitButton from "../buttons/yellowSubmitButton/YellowSubmitButton";
+import FindStoreButton from "../buttons/findstoreButton/findstoreButton";
 
 const BlogPost = ({ readMoreRoute }) => {
   const router = useRouter();
@@ -15,6 +17,10 @@ const BlogPost = ({ readMoreRoute }) => {
       router.push(route);
     }
   };
+  const [filteredData, setFilteredData] = useState(Data); // State to hold the filtered data
+  const [inputValue, setInputValue] = useState("");
+
+  
 
   const itemsPerPage = 6;
   const [currentPage, setCurrentPage] = useState(1);
@@ -24,8 +30,31 @@ const BlogPost = ({ readMoreRoute }) => {
   useEffect(() => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    setDisplayedData(Data.slice(startIndex, endIndex));
-  }, [currentPage]);
+    setDisplayedData(filteredData.slice(startIndex, endIndex));
+  }, [currentPage, filteredData]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+    const filtered = Data.filter((item) =>
+      item.title.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredData(filtered);
+    setCurrentPage(1); // Reset to the first page after search
+  };
+
+  // const handleSearch = () => {
+  //   const filtered = Data.filter((item) =>
+  //     item.title.toLowerCase().includes(inputValue.toLowerCase())
+  //   );
+  //   setFilteredData(filtered);
+  //   setCurrentPage(1); // Reset to the first page after search
+  // };
+
+  const clearInput = () => {
+    setInputValue("");
+    setFilteredData(Data); // Reset to all data when input is cleared
+    setCurrentPage(1); // Reset to the first page when the input is cleared
+  };
 
   // const handlePageChange = (event, value) => {
   //   setCurrentPage(value);
@@ -84,17 +113,63 @@ const BlogPost = ({ readMoreRoute }) => {
   // };
 
   return (
-    <div ref={projectsRef} >
-      <div className="BlogsInner">
-      {displayedData.map((item, index) => (
-        <div  key={index}  >
-          <BlogsCards
-            item={item}
-            handleReadMoreClick={handleReadMoreClick}
+    <div ref={projectsRef}>
+       <div className="SearchMainContainer">
+        <div className="inputContainer">
+          <svg
+            className="searchIcon"
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          >
+            <path
+              d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"
+              fill="#5B3524"
+            />
+          </svg>
+          <input
+            className="input"
+            name="text"
+            placeholder="Search Blogs"
+            type="search"
+            value={inputValue}
+            onChange={handleInputChange}
+            // onKeyPress={(e) => e.key === "Enter" && handleSearch()} 
           />
+          {inputValue && (
+            <button className="close-btn" onClick={clearInput}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="close-icon"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          )}
         </div>
-        
-      ))}
+      </div>
+      <div className="BlogsInner">
+      {filteredData.length > 0 ? (
+          displayedData.map((item, index) => (
+            <div key={index}>
+              <BlogsCards
+                item={item}
+                handleReadMoreClick={handleReadMoreClick}
+                // handleReadMoreClick={() => handleReadMoreClick(item.route)}
+              />
+            </div>
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
       </div>
 
       <div>
