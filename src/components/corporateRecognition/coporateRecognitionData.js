011a5ac68b2c0a01a6f40740fa7744ef4ef06d70
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-08-14 16:11:47 +0530

    corporate social responsibility

---
+++ src/components/corporateRecognition/coporateRecognitionData.js
@@ -0,0 +1,9 @@
+import RecognitionImage from "../../images/recognition.png";
+
+const CorporateRecognitionData = [
+    {image : RecognitionImage , description : "Gold award in the CSR Covid relief project category."},
+    {image : RecognitionImage , description : "5 years on the Civic 50 list, in recognition of our social impact"},
+    {image : RecognitionImage , description : "ET Awards’ Corporate Citizen of the Year TCS"},
+    {image : RecognitionImage , description : "CSR China top 100 award for our long-term, successful STEM program."},
+]
+export default CorporateRecognitionData;
