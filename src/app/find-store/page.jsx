797207919feef8b9096ba0e4fb8f92128d2dade2
jeel1802 <160886657+jeel1802@users.noreply.github.com>
2024-08-14 19:30:16 +0530

    dfed

--- src/app/find-store/page.jsx
+++ src/app/find-store/page.jsx
@@ -1,9 +1,23 @@
+"use client"
+import FindStoreHeader from "@/components/findstoreheader/page";
 import FindStore from "../../components/findStore_search/page"
+import { useRef } from "react";
 
 const FindStoreSearch =() =>{
+    const section1 = useRef();
+    const section2 = useRef();
+    function scrollTo(section) {
+      section.current.scrollIntoView({ behavior: "smooth" });
+    }
     return(
         <div>
+            <div ref={section1}>
+                <FindStoreHeader goToSectionRef={section2} scrollTo={scrollTo}/>
+            </div>
+            <div ref={section2}>
             <FindStore />
+
+            </div>
         </div>
     )
 }
