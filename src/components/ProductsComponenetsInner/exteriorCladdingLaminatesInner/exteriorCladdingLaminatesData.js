5a6d51fb4899960002bcd7e6ad1ead0dfc58d975
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-09-11 13:43:10 +0530

    new category changes -j

---
+++ src/components/ProductsComponenetsInner/exteriorCladdingLaminatesInner/exteriorCladdingLaminatesData.js
@@ -0,0 +1,140 @@
+import icon1 from "../../../images/Icons-11-2048x2048.png";
+import icon2 from "../../../images/Icons-12.png";
+import Image1 from "../../../images/1_LOW-ELECTRICAL-RESISTANT.svg";
+import Productimg1 from "../../../images/5-2048x749.png";
+// import products from "@/components/aboutUs_products/productData";
+// import verticalIcon from "../../images/vertical-icon.svg";
+// import horizontalIcon from "../../images/horizontal-icon.svg";
+// import laboratoriesIcon from "../../images/laboratories-icon.svg";
+// import electronicIndustriesIcon from "../../images/electronic-industries-icon.svg";
+// import pharmaceuticalLabsIcon from "../../images/pharmaceutical-labs-icon.svg";
+// import itOfficesIcon from "../../images/it-offices-icon.svg";
+// import callCentresIcon from "../../images/call-centres-icon.svg";
+
+const ExteriorCladdingLaminatesData = {
+  header: "Exterior cladding (XCL)",
+  description: `Discover Crown Decor’s XCL - Exterior Compact Laminate, a revolutionary high-pressure
+laminate accurately crafted from multiple layers of kraft papers. Boasting thicknesses ranging
+from 2mm to 25mm, this laminate features a captivating decorative face on both sides,
+shielded by a UV protective film to withstand the harshest sun exposure. Whether it's for
+Building Facades/Cladding, Balconies, Verandah, or Outdoor Benches, XCL is the ultimate
+choice. With an impressive density of 1.45gm/cm3, these compacts are not only incredibly
+sturdy and resistant to damage, but they also exhibit exceptional structural stability, eliminating
+the need for substrate support in thicknesses over 6mm. Elevate your projects with the
+unparalleled durability and aesthetic appeal of Crown Decor’s XCL - Exterior Compact Laminate.`,
+  boxes: [
+    {
+      icon: icon1,
+      title: "SHEET THICKNESS",
+      details: ["6mm" , "8mm" , "10mm"],
+    },
+    {
+      icon: icon2,
+      title: "SHEET SIZE",
+      details: ["2440 x 1220 mm (A)", "3050 x 1220 mm (B)", "3050 x 1300 mm* (C)", "*(Available in selected colours)"],
+    },
+  ],
+  features: [
+    {
+      image1: Image1,
+      description: "Water Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Heat Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Resistant to Cigarette Burns",
+    },
+    {
+      image1: Image1,
+      description: "Scratch Resistant",
+    },
+    {
+      image1: Image1,
+      description: "ColourFast",
+    },
+    {
+      image1: Image1,
+      description: "Impact Retardant",
+    },
+    {
+      image1: Image1,
+      description: "Environmental Friendly",
+    },
+    {
+      image1: Image1,
+      description: "Stain Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Sleek Modern Design",
+    },
+    {
+      image1: Image1,
+      description: "Rot Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Stability",
+    },
+    {
+      image1: Image1,
+      description: "Suitable for contract use",
+    },
+    {
+      image1: Image1,
+      description: "Flame Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Weather Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Indoor and Outdoor use",
+    },
+    {
+      image1: Image1,
+      description: "Easy to clean",
+    },
+    {
+      image1: Image1,
+      description: "Minimum Maintenance",
+    },
+
+  ],
+  products: [
+    {
+      ProductImg: Productimg1,
+    }
+  ],
+  application: {
+    alignment: [
+      {
+        label: "Vertical",
+        description: "Vertical alignment description here.",
+      },
+      {
+        label: "Horizontal",
+        description: "Horizontal alignment description here.",
+      },
+    ],
+    suitableFor: [
+      { label: "Laboratories", description: "Laboratories description here." },
+      {
+        label: "Electronic Industries",
+        description: "Electronic Industries description here.",
+      },
+      {
+        label: "Pharmaceutical Labs",
+        description: "Pharmaceutical Labs description here.",
+      },
+      { label: "IT Offices", description: "IT Offices description here." },
+      { label: "Call Centres", description: "Call Centres description here." },
+    ],
+  },
+};
+
+export default ExteriorCladdingLaminatesData;
---
+++ src/components/ProductsComponenetsInner/interiorCladdingLaminatesInner/interiorCladdingLaminatesData.js
@@ -0,0 +1,109 @@
+import icon1 from "../../../images/Icons-11-2048x2048.png";
+import icon2 from "../../../images/Icons-12.png";
+import Image1 from "../../../images/1_LOW-ELECTRICAL-RESISTANT.svg";
+import Productimg1 from "../../../images/5-2048x749.png";
+// import products from "@/components/aboutUs_products/productData";
+// import verticalIcon from "../../images/vertical-icon.svg";
+// import horizontalIcon from "../../images/horizontal-icon.svg";
+// import laboratoriesIcon from "../../images/laboratories-icon.svg";
+// import electronicIndustriesIcon from "../../images/electronic-industries-icon.svg";
+// import pharmaceuticalLabsIcon from "../../images/pharmaceutical-labs-icon.svg";
+// import itOfficesIcon from "../../images/it-offices-icon.svg";
+// import callCentresIcon from "../../images/call-centres-icon.svg";
+
+const InteriorCladdingLaminatesData = {
+  header: "Interior cladding",
+  description: `Crown Decor’s Interior cladding compact laminate stands as a high-density, resilient material
+carefully designed for wall cladding and other interior applications where strength, aesthetics,
+and easy maintenance take precedence. Fashioned by compressing multiple layers of kraft
+paper saturated with phenolic resin under intense heat and pressure, this self-supporting
+laminate eliminates the need for additional backing or substrate. Its top surface features a
+decorative layer that expertly replicates various materials such as wood, stone, and metal, or
+showcases modern abstract designs and solid colors.`,
+  description1: `Engineered for the most demanding environments including hospitals, schools, airports, and
+commercial spaces, our interior cladding compact laminate exhibits exceptional resistance to
+impact, moisture, and scratches. Its bacteria-resistant, easy-to-clean, and non-porous properties
+make it the ultimate choice for spaces requiring rigorous hygiene standards, such as restrooms,
+kitchens, and healthcare facilities. Available in a rich array of thicknesses, colors, and finishes,
+this material unlocks endless design possibilities while delivering a seamless, modern aesthetic.
+Its unwavering durability and design adaptability have firmly positioned interior cladding
+compact laminate as the top choice for creating enduring and stylish interior surfaces.`,
+  boxes: [
+    {
+      icon: icon1,
+      title: "SHEET THICKNESS",
+      details: "0.6 mm to 1.5mm",
+    },
+    {
+      icon: icon2,
+      title: "SHEET SIZE",
+      details: ["1220 x 2440 mm", "mm", "mm", "mm"],
+    },
+  ],
+  features: [
+    {
+      image1: Image1,
+      description: "Aesthetically Beautiful",
+    },
+    {
+      image1: Image1,
+      description: "Scratch Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Solvent Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Impact Resistant",
+    },
+    {
+      image1: Image1,
+      description: "Heat Retardant",
+    },
+    {
+      image1: Image1,
+      description: "Fire Retardant",
+    },
+    {
+      image1: Image1,
+      description: "Easy to clean & maintain",
+    },
+    {
+      image1: Image1,
+      description: "Sustainable",
+    },
+  ],
+  products: [
+    {
+      ProductImg: Productimg1,
+    }
+  ],
+  application: {
+    alignment: [
+      {
+        label: "Vertical",
+        description: "Vertical alignment description here.",
+      },
+      {
+        label: "Horizontal",
+        description: "Horizontal alignment description here.",
+      },
+    ],
+    suitableFor: [
+      { label: "Laboratories", description: "Laboratories description here." },
+      {
+        label: "Electronic Industries",
+        description: "Electronic Industries description here.",
+      },
+      {
+        label: "Pharmaceutical Labs",
+        description: "Pharmaceutical Labs description here.",
+      },
+      { label: "IT Offices", description: "IT Offices description here." },
+      { label: "Call Centres", description: "Call Centres description here." },
+    ],
+  },
+};
+
+export default InteriorCladdingLaminatesData;
