88dc402cc621367b5e50e5a6202a00533eedd978
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-10-03 16:22:41 +0530

    upd category -j

--- src/components/categoryLeftRight/CategoryLeftRight.jsx
+++ src/components/categoryLeftRight/CategoryLeftRight.jsx
@@ -11,23 +11,24 @@ import logo1 from "@/images/xylem.png";
 import logo2 from "@/images/crown.png";
 import logo3 from "@/images/crown_qbiss.png";
 import logo4 from "@/images/crownXCL.png";
+import logo5 from "@/images/svgLogos/FinalHeaderLogo.svg";
 import CategoryLeftRightItem from "./categoryLeftRightItem.jsx/CategoryLeftRightItem";
 import "./categoryLeftRight.scss"
 
 const categoryLeftRightData = [
     {
         title:"XYLEM",
-        text:"Our Decorative Laminates are produced by impregnating multitude of papers including tissue paper, decor paper, barrier paper, kraft paper with resin. The resulting sandwich is fused underheat and pressure. The fusion process creates strong bonds that contribute to our laminates'exceptional durability. ",
+        text:"Step into the world of Xylem, where innovation is at the heart of everything we do. Xylem represents our premium-grade decorative laminates, meticulously crafted to elevate your surroundings. Each laminate embodies inspiring, high quality, and artistic excellence designed to define your unique style. From curating unique designs to creating exclusive laminates, we are on a mission to bring your vision to life.",
         url:"/products#xylem",
         image1:image1,
         image2:image2,
         image3:image3,
         logo1:logo1,
-        logo2:logo2,
+        
     },
     {
         title:"QBISS",
-        text:"Qbiss is a high pressure structural laminate, built up from multiple papers of kraft papers to produce laminate in thickness ranging from 2mm to 25mm. ",
+        text:"Qbiss is a high-pressure structural laminate made from multiple layers of kraft papers, with a thickness range from 2mm to 25mm. Its decorative face on both sides makes it suitable for interior applications like washroom cubicles, locker doors, wall panels, and laboratory furniture. With a density of 1.45gm/cm3, our compacts are exceptionally resilient and require no substrate support in thicknesses over 6mm.",
         url:"/products#Qbiss",
         image1:image4,
         image2:image5,
@@ -45,21 +46,22 @@ const categoryLeftRightData = [
     },
     {
         title:"ROYAL CROWN",
-        text:"XCL- Exterior Compact Laminate is a high pressure laminate, built up from multiple papers of kraft papers to produce laminate in thickness ranging from 2mm to 25mm. ",
+        text:"Royal Crown Laminates takes pride in its rich legacy of innovation, cutting-edge technology, and expertise, offering over 450 trendsetting surface designs. Our collection of modern laminates boasts a wide range of finishes and textures in 1mm thickness, empowering you to effortlessly realize your dream decor.",
         url:"/products#royal-crown",
         image1:image7,
         image2:image8,
         image3:image9,
-        logo1:logo4,
+        logo1:logo5,
     },
     {
         title:"CROWN",
-        text:"XCL- Exterior Compact Laminate is a high pressure laminate, built up from multiple papers of kraft papers to produce laminate in thickness ranging from 2mm to 25mm. ",
+        text:"Crown's Lean Line offers an exquisite and cost-effective range of laminates in a variety of designs, colors, and textures, all in 0.8mm thickness. Manufactured at our highly advanced production facility, the Lean Line guarantees a consistent and exceptional level of quality.",
         url:"/products#crown",
         image1:image7,
         image2:image8,
         image3:image9,
-        logo1:logo4,
+        // logo1:logo4,
+        logo1:logo2,
     },
 ]
 
