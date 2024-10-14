e540e44b68681e72219978b2f7d03c10815cfdc6
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-10-14 11:27:19 +0530

    jlkl

--- src/components/headerNew/Header.jsx
+++ src/components/headerNew/Header.jsx
@@ -139,7 +139,7 @@ const Header = () => {
           >
             <LinkHover
               url={"/products"}
-              text={"Products"}
+              text={"Category"}
               fontSize={"16px"}
               isHomePage={isHome}
             />
@@ -694,7 +694,7 @@ const Header = () => {
           >
             <LinkHover
               url={"/category"}
-              text={"Category"}
+              text={"Products"}
               fontSize={"16px"}
               isHomePage={isHome}
             />
