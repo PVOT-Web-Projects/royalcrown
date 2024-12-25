093e7df8dde7b7c66f860361f2da2f4962966d51
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-12-25 17:42:44 +0530

    UPD --J

---
+++ src/app/crown/page.jsx
@@ -0,0 +1,24 @@
+
+"use client"
+import { useEffect } from "react";
+import SocialPage from "@/components/socialPage/socialpage";
+import Form from "@/components/forms/form1/Form";
+import SocialMediaImg from "@/images/socials-image.png";
+import CategoryLeftRightCat from "@/components/categoryLeftRight/CategoryLeftRightOne";
+import "./crownPage.scss"
+// import ProductsInner from "@/components/ProductsComponenetsInner/decorativeLaminatesInner/page";
+const XylemPage = () => {
+  useEffect(() => {
+    document.title = "Xylem | Royal Crown";
+  });
+  return (
+    <>
+    <CategoryLeftRightCat />
+    {/* <div className="Form_Page">
+        <SocialPage socialMediaImgSrc={SocialMediaImg} />
+        <Form />
+      </div> */}
+    </>
+  );
+};
+export default XylemPage;
