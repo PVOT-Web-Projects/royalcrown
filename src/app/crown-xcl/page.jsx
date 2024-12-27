ede87c4f5a3db54819621dfa0018cbf3ac51a98c
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-12-27 11:46:03 +0530

    upd catg -changes --j

--- src/app/crown-xcl/page.jsx
+++ src/app/crown-xcl/page.jsx
@@ -4,16 +4,17 @@ import { useEffect } from "react";
 import SocialPage from "@/components/socialPage/socialpage";
 import Form from "@/components/forms/form1/Form";
 import SocialMediaImg from "@/images/socials-image.png";
-import CategoryLeftRightCat from "@/components/categoryLeftRight/CategoryLeftRightOne";
+// import CategoryLeftRightCat from "@/components/categoryLeftRight/CategoryLeftRightOne";
+import AboutUsProductsCrownXcl from "@/components/aboutUs_productscrownXcl/page"
 import "./crownXclPage.scss"
 // import ProductsInner from "@/components/ProductsComponenetsInner/decorativeLaminatesInner/page";
 const XylemPage = () => {
   useEffect(() => {
-    document.title = "Xylem | Royal Crown";
+    document.title = "Crown XCL | Royal Crown";
   });
   return (
     <>
-    <CategoryLeftRightCat />
+    <AboutUsProductsCrownXcl />
     {/* <div className="Form_Page">
         <SocialPage socialMediaImgSrc={SocialMediaImg} />
         <Form />
