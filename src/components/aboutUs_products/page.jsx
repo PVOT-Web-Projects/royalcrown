bcd283581de0761d8f3a4b5bee54e0c12049abcc
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-03-19 16:21:41 +0530

    ds

--- src/components/aboutUs_products/page.jsx
+++ src/components/aboutUs_products/page.jsx
@@ -707,8 +707,8 @@ const Page = () => {
                     border: "1px solid #5b3524",
                     color: "#5b3524",
                     margin: "0 10px",
-                    padding: "18px 13px",
-                    fontSize: "20px",
+                    padding: "14px 10px",
+                    fontSize: "15px",
                     borderRadius: "0px",
                     transition: "background-color 0.3s, color 0.3s",
 
@@ -727,8 +727,8 @@ const Page = () => {
                     "&.Mui-selected": {
                       backgroundColor: "#5b3524",
                       margin: "0 10px",
-                      padding: "18px 13px",
-                      fontSize: "20px",
+                      padding: "14px 10px",
+                      fontSize: "15px",
                       color: "white",
                       border: "none",
 
