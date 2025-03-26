5dc1373b79835938537634b91d2a8362a9f8a09f
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2025-03-26 13:49:11 +0530

    chnages --j

--- src/components/productinfoslider/page.jsx
+++ src/components/productinfoslider/page.jsx
@@ -163,7 +163,7 @@ export default function ProductInfoSlider() {
 
       const link = document.createElement("a");
       link.href = blobUrl;
-      link.download = activeImage.split("/").pop(); 
+      link.download = activeImage.split("/").pop();
       link.click();
 
       URL.revokeObjectURL(blobUrl);
@@ -231,7 +231,9 @@ export default function ProductInfoSlider() {
                       nextEl: ".swiper-button-next-product",
                       prevEl: ".swiper-button-prev-product",
                     }}
-                    onSlideChange={(swiper) => setActiveSlideIndex(swiper.activeIndex)} // Track active slide index
+                    onSlideChange={(swiper) =>
+                      setActiveSlideIndex(swiper.activeIndex)
+                    } // Track active slide index
                   >
                     {slide.images.map((image, idx) => (
                       <SwiperSlide key={idx}>
@@ -247,39 +249,30 @@ export default function ProductInfoSlider() {
                     ))}
                   </Swiper>
                   <div className="swiper-button-next-product">
-                    <svg
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
-                      stroke-linejoin="round"
-                      stroke-miterlimit="2"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
+                  <svg
                       width="30"
                       height="30"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      
                     >
-                      <path
-                        d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z"
-                        fill-rule="nonzero"
-                        fill="#5B3524"
-                      ></path>
+                      <path fill="#5B3524" d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
                     </svg>
+                    
                   </div>
                   <div className="swiper-button-prev-product">
-                    <svg
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
-                      stroke-linejoin="round"
-                      stroke-miterlimit="2"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="30"
+                  <svg
                       width="30"
+                      height="30"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
                     >
                       <path
-                        d="m9.474 5.209s-4.501 4.505-6.254 6.259c-.147.146-.22.338-.22.53s.073.384.22.53c1.752 1.754 6.252 6.257 6.252 6.257.145.145.336.217.527.217.191-.001.383-.074.53-.221.293-.293.294-.766.004-1.057l-4.976-4.976h14.692c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.692l4.978-4.979c.289-.289.287-.761-.006-1.054-.147-.147-.339-.221-.53-.221-.191-.001-.38.071-.525.215z"
-                        fill-rule="nonzero"
                         fill="#5B3524"
-                      ></path>
+                        d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"
+                      />
                     </svg>
                   </div>
                   {/* <Image
